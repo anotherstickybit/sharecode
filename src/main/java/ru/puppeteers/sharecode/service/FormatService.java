06d1be5a6f102e12bef01e24813ead6f3e7aac57
Marat Sadykov <marats93@yandex.ru>
2023-05-25 19:59:22 +0300

    [ref]

--- src/main/java/ru/puppeteers/sharecode/service/FormatService.java
+++ src/main/java/ru/puppeteers/sharecode/service/FormatService.java
@@ -1,8 +1,6 @@
 package ru.puppeteers.sharecode.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.puppeteers.sharecode.api.response.FormatResponse;
@@ -17,25 +15,9 @@ public class FormatService {
 
     private final FormatRepository formatRepository;
     private final FormatMapper formatMapper;
-    @Autowired
-    private final CacheManager cacheManager;
 
     @Cacheable(cacheNames = "formats")
     public List<FormatResponse> getAll() {
         return formatRepository.findAll().stream().map(formatMapper::toResponse).toList();
     }
- //вроде как надо добавить 2й вариант метода и еще добавить еще один гетмаппер . пытался понять как работает.
-    // в примере на гитхабе там было 2 метода в сервисе и 2 маппера.
-    // public List<FormatResponse> getAll2() {
-    //        if(cacheManager.getCache("formats2").get(customerId) != null) {
-    //            return cacheManager.getCache("formats2").get(???).get().toString();
-    //        }
-    //
-    //        cacheManager.getCache("formats2").put(??, ??);
-    //
-    //        return ??;
-    //    }
-
-
-
 }
